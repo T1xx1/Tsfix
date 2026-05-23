34ec65789dbe6398a80b6cb8e0fe52b357a631fa
Tiziano <69322987+T1xx1@users.noreply.github.com>
2026-05-23 22:14:57 +0200

    feat: assert

--- src/assert.ts
+++ src/assert.ts
@@ -1,3 +1,7 @@
+export const assert = <T>(arg: T): never => {
+	throw new Error(`Assertion failed: ${arg}`);
+};
+
 export const assertUnreachable = (arg: never): never => {
 	throw new Error(`Unreachable case: ${arg}`);
 };
